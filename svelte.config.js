c799a2f253e03b227fbc7cff41a3091cb9df6823
amin <bandar3eda021@gmail.com>
2023-01-31 11:09:53 +0330

    configure missed dependencies

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			out: 'dist',
+			pages: 'build',
+			assets: 'build'
+		})
+	},
+	preprocess: vitePreprocess()
+};
+
+export default config;
